aa60c130a351c23511210dba553a1891b8c45da1
samueleaton <sameaton11@gmail.com>
2015-08-23 22:49:34 -0600

    'on' method now retunrs a method object

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,59 @@ var rename = require("gulp-rename");
 var replace = require("gulp-replace");
 var uglify = require("gulp-uglify");
 var plumber = require("gulp-plumber");
+var eslint = require("gulp-eslint");
 
-gulp.task("production", function(){
+gulp.task("production", ["linter"], function(){
 	gulp.src("src/eve.js")
+	.pipe(plumber())
+	// .pipe(remove({production: true}))
+	// .pipe(replace("//removeIf(development)", ""))
+	// .pipe(replace("//endRemoveIf(development)", ""))
+	.pipe(gulp.dest("./lib"))
+	.pipe(uglify())
+	.pipe(rename("eve.min.js"))
+	.pipe(gulp.dest("./lib"));
+});
+
+// gulp.task("development", ["devLinter"], function(){
+// 	gulp.src("src/eve.js")
+// 	.pipe(plumber())
+// 	.pipe(remove({development: true}))
+// 	.pipe(replace("//removeIf(production)", ""))
+// 	.pipe(replace("//endRemoveIf(production)", ""))
+// 	.pipe(rename("eve.dev.js"))
+// 	.pipe(gulp.dest("./lib/dev"))
+// 	.pipe(uglify())
+// 	.pipe(rename("eve.dev.min.js"))
+// 	.pipe(gulp.dest("./lib/dev"));
+// });
+
+// gulp.task("devLinter", function(){
+// 	console.log("DEV LINTER");
+// 	return gulp.src("src/eve.js")
+// 	.pipe(plumber())
+// 	.pipe(remove({development: true}))
+// 	.pipe(replace("//removeIf(production)", ""))
+// 	.pipe(replace("//endRemoveIf(production)", ""))
+// 	.pipe(eslint({"configFile":".eslintrc"}))
+// 	.pipe(eslint.format())
+// 	.pipe(eslint.failOnError());
+// });
+
+gulp.task("linter", function(){
+	console.log("* ESLint ON *");
+	return gulp.src("src/eve.js")
 	.pipe(plumber())
 	.pipe(remove({production: true}))
 	.pipe(replace("//removeIf(development)", ""))
 	.pipe(replace("//endRemoveIf(development)", ""))
-	.pipe(gulp.dest("./lib/production"))
-	.pipe(uglify())
-	.pipe(rename("eve.min.js"))
-	.pipe(gulp.dest("./lib/production"));
-});
-
-gulp.task("development", function(){
-	gulp.src("src/eve.js")
-	.pipe(plumber())
-	.pipe(remove({development: true}))
-	.pipe(replace("//removeIf(production)", ""))
-	.pipe(replace("//endRemoveIf(production)", ""))
-	.pipe(rename("eve.dev.js"))
-	.pipe(gulp.dest("./lib/dev"))
-	.pipe(uglify())
-	.pipe(rename("eve.dev.min.js"))
-	.pipe(gulp.dest("./lib/dev"));
+	.pipe(eslint({"configFile":".eslintrc"}))
+	.pipe(eslint.format())
+	.pipe(eslint.failOnError());
 });
 
 gulp.task("watch", function(){
-	gulp.watch("src/eve.js", ["production", "development"]);
+	gulp.watch("src/eve.js", ["production"]);
 });
 
-gulp.task("default", ["production", "development", "watch"]);
+gulp.task("default", ["production", "watch"]);
